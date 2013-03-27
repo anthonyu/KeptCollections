3e02f348a7ee78064192de96fbdc3b15e2ac4e24
Anthony D. Urso <anthonyu@killa.net>
2013-03-27 12:28:11 -0700

    Set up zookeeper in process to ease testing.

--- src/test/java/net/killa/kept/BaseKeptUtil.java
+++ src/test/java/net/killa/kept/BaseKeptUtil.java
@@ -17,28 +17,51 @@
  */
 package net.killa.kept;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.server.NIOServerCnxn;
+import org.apache.zookeeper.server.ZooKeeperServer;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class BaseKeptUtil {
+    private static final int CLIENT_PORT = new Random().nextInt(55535) + 10000;
+
+    private static NIOServerCnxn.Factory nioServerCnxnFactory;
+
     protected String parent;
     protected ZooKeeper keeper;
+
+    @BeforeClass
+    public static void init() throws IOException, InterruptedException {
+	final File dir = new File(System.getProperty("java.io.tmpdir"),
+		"zookeeper").getAbsoluteFile();
+	final ZooKeeperServer server = new ZooKeeperServer(dir, dir, 2000);
+
+	BaseKeptUtil.nioServerCnxnFactory = new NIOServerCnxn.Factory(
+		new InetSocketAddress(BaseKeptUtil.CLIENT_PORT), 5000);
+
+	BaseKeptUtil.nioServerCnxnFactory.startup(server);
+    }
 
     @Before
     public void before() throws IOException, InterruptedException,
 	    KeeperException {
 	CountDownLatch latch = new CountDownLatch(1);
 
-	// FIXME: set up a zookeeper server in process
 	CountDownOnConnectWatcher watcher = new CountDownOnConnectWatcher();
 	watcher.setLatch(latch);
-	this.keeper = new ZooKeeper("localhost:2181", 20000, watcher);
+	this.keeper = new ZooKeeper("localhost:"
+		+ Integer.toString(BaseKeptUtil.CLIENT_PORT), 20000, watcher);
 	if (!latch.await(5, TimeUnit.SECONDS)) {
 	    throw new RuntimeException("unable to connect to server");
 	}
@@ -50,5 +73,10 @@ public class BaseKeptUtil {
 	    this.keeper.delete(this.parent + '/' + s, -1);
 	}
 	this.keeper.close();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+	BaseKeptUtil.nioServerCnxnFactory.shutdown();
     }
 }
